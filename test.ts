f23ac51626056d837a8a453b1412f16b5b6b0a2e
SeanBurford <SeanBurford@users.noreply.github.com>
2019-01-18 16:50:44 +1100

    Fix test

--- test.ts
+++ test.ts
@@ -1,6 +1,5 @@
 let coeff: number[] = [0, 34197, 30040, 41947, 20137, 32329, 28398];
 let ms5803 = MS5803.create(MS5803.Addr.I2C77);
-let mprls = MPRLS.create(MPRLS.Addr.I2C18);
 
 // Test conversion of D1 and D2 with known coefficients
 let reading = new MS5803.Reading(5173016, 8509400, coeff);
@@ -24,6 +23,5 @@ control.assert(result.temperature == -26017,
 control.assert(result.pressure == -81382,
     "pressure " + result.pressure + " != -81382");
 
-// Test MPRLS with raw pressure=0
+let mprls = MPRLS.create(MPRLS.Addr.I2C18);
 let pressure = mprls.query()
-control.assert(pressure == 0, "pressure " + pressure + " != 0");
