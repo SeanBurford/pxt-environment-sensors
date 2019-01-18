4dce3bacef251b2f46208eda54fa02a5d51aae43
SeanBurford <SeanBurford@users.noreply.github.com>
2019-01-18 16:49:18 +1100

    MPRLS tested with hardware.

--- main.ts
+++ main.ts
@@ -27,28 +27,44 @@ namespace MPRLS {
         i2cAddr: Addr;
         readonly debug: number;
 
+        readonly pMin: number;
+        readonly pMax: number;
+        readonly outputMax: number;
+        readonly outputMin: number;
+
         constructor(i2cAddr: Addr = Addr.I2C18) {
             this.i2cAddr = i2cAddr;
             this.debug = 1;
+
+            this.pMin = 1677722;
+            this.pMax = 15099494;
+            this.outputMin = 0;
+            this.outputMax = 25;
         }
 
         protected readPressure(): number {
             // Request the pressure.
-            pins.i2cWriteNumber(this.i2cAddr, 0xAA, NumberFormat.UInt8BE, true);
-            pins.i2cWriteNumber(this.i2cAddr, 0x00, NumberFormat.UInt8BE, true);
-            pins.i2cWriteNumber(this.i2cAddr, 0x00, NumberFormat.UInt8BE, false);
+            //pins.i2cWriteNumber(this.i2cAddr, 0xAA, NumberFormat.UInt8BE, true);
+            //pins.i2cWriteNumber(this.i2cAddr, 0x00, NumberFormat.UInt8BE, true);
+            //pins.i2cWriteNumber(this.i2cAddr, 0x00, NumberFormat.UInt8BE, false);
+            pins.i2cWriteNumber(this.i2cAddr, 0xAA000000, NumberFormat.UInt32BE, false);
 
             // Monitor the status byte
-            for (let attempt = 0; attempt < 50; attempt++) {
+            let attempt = 0;
+            for (attempt = 0; attempt < 50; attempt++) {
                 let stat = pins.i2cReadNumber(this.i2cAddr, NumberFormat.UInt8BE, false);
                 if ((stat & STATUS.MPRLS_STATUS_BUSY) == 0) {
                     break;
                 }
                 basic.pause(10);
             }
+            //serial.writeValue("attempts", attempt);
 
             let val = pins.i2cReadNumber(this.i2cAddr, NumberFormat.UInt32BE, false);
-            let stat = (val & 0xFF000000) >> 24;
+            let stat = (val >> 24) & 0xFF;
+            let v1 = (val >> 16) & 0xFF;
+            let v2 = (val >> 8) & 0xFF;
+            let v3 = (val) & 0xFF;
             if (stat & STATUS.MPRLS_STATUS_BUSY) {
                 return 0xFFFFFFF1;
             }
@@ -58,7 +74,8 @@ namespace MPRLS {
             if (stat & STATUS.MPRLS_STATUS_ERROR) {
                 return 0xFFFFFFF3;
             }
-            return val & 0x00FFFFFF;
+
+            return (v1 << 16) | (v2 << 8) | v3;
         }
 
         /**
@@ -72,7 +89,9 @@ namespace MPRLS {
             if (raw >= 0xFFFFFFF0) {
                 return -1;
             }
-            return raw;
+            let psi = (((raw - this.pMin) * (this.outputMax - this.outputMin)) / (this.pMax - this.pMin)) + this.outputMin;
+            let hpa = psi * 68.947572932;
+            return hpa;
         }
     }
 
